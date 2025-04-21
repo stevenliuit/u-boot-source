e95c77bf6ace4a34318811493fe462c7caa21a73
Steven <stevenliuit@gmail.com>
2025-04-21 19:55:58 +0800

    添加缺失文件alark35-3500

---
+++ include/configs/alark35_3500.h
@@ -0,0 +1,11 @@
+/* SPDX-License-Identifier: GPL-2.0+ */
+
+#ifndef __ALARK35_3500_H
+#define __ALARK35_3500_H
+
+#define ROCKCHIP_DEVICE_SETTINGS \
+	"cramfsaddr=0x0c000000\0"
+
+#include <configs/rk3568_common.h>
+
+#endif
