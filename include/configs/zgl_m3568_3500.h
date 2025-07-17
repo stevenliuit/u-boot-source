3328d539a8e47fb34ee2d138c4c6e6637d3bb075
Steven <stevenliuit@gmail.com>
2025-07-17 16:35:37 +0800

    添加zgl-m3568  board
    添加zgl-m3568  board

--- include/configs/zgl_m3568_3500.h
+++ include/configs/zgl_m3568_3500.h
@@ -1,7 +1,7 @@
 /* SPDX-License-Identifier: GPL-2.0+ */
 
-#ifndef __ZLG_M3568_3500_H
-#define __ZLG_M3568_3500_H
+#ifndef __ZLG_M3568_H
+#define __ZLG_M3568_H
 
 #define ROCKCHIP_DEVICE_SETTINGS \
 	"cramfsaddr=0x0c000000\0"
